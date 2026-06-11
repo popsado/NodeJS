7d4b46a4779ab4ca85c5498b979840a03afa2bc1
popsado <popsado@hotmail.com>
2026-06-11 12:50:54 +0700

    commit public
    commit public

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require("express");
 const chalk = require("chalk");
 const debug = require("debug")("app");
 const morgan = require("morgan");
+const path = require("path");
+
 const app = express();
 const port = 3000;
 app.use(morgan("combined"));
 
+app.use(express.static(path.join(__dirname, '/public/')));
+
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    //res.send("Hello World!");
+    res.send("<span style='color: red;'>Hello World!</span>")
 })
 
 app.listen(port, () => {
